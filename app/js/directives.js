0ad7a1d89bb1c8c263b30fed04e2ebbbf0767a62
iampeterbanjo <me@iampeterbanjo.com>
2014-06-22 00:34:31 +0100

    pass directives test

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,24 +1,12 @@
-﻿(function() {
-    'use strict';
+﻿(function () {
+	'use strict';
 
-    // Define the directive on the module.
-    // Inject the dependencies. 
-    // Point to the directive definition function.
-    angular.module('myApp').directive('directive', ['$window', directive]);
-    
-    function directive ($window) {
-        // Usage:
-        // 
-        // Creates:
-        // 
-        var directive = {
-            link: link,
-            restrict: 'A'
-        };
-        return directive;
-
-        function link(scope, element, attrs) {
-        }
-    }
-
+	// Define the directive on the module.
+	// Inject the dependencies. 
+	// Point to the directive definition function.
+	angular.module('myApp').directive('appVersion', ['version', function (version) {
+		return function (scope, element, attrs) {
+			element.text(version)
+		}
+	}])
 })();
