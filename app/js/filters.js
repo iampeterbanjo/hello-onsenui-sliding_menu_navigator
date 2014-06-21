c9b839ee0b62bad5133e78a4baab8a2e92cd0aee
iampeterbanjo <me@iampeterbanjo.com>
2014-06-22 00:46:41 +0100

    pass filter and services test

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,24 +1,12 @@
-﻿(function() {
-    'use strict';
+﻿(function () {
+	'use strict';
 
-    // Define the directive on the module.
-    // Inject the dependencies. 
-    // Point to the filter definition function.
-    angular.module('myApp').filter('filters', ['$window', filter]);
-    
-    function filter ($window) {
-        // Usage:
-        // 
-        // Creates:
-        // 
-        var filter = {
-            link: link,
-            restrict: 'A'
-        };
-        return filter;
-
-        function link(scope, element, attrs) {
-        }
-    }
-
+	// Define the filters on the module.
+	// Inject the dependencies. 
+	// Point to the filter definition function.
+	angular.module('myApp').filter('interpolate', ['version', function (version) {
+		return function (text) {
+			return String(text).replace(/\%VERSION\%/mg, version)
+		}
+	}])
 })();
--- app/js/services.js
+++ app/js/services.js
@@ -4,10 +4,6 @@
     // Define the directive on the module.
     // Inject the dependencies. 
     // Point to the filter definition function.
-    angular.module('myApp').service('services', ['$window', service]);
-    
-    function service ($window) {
-
-    }
+    angular.module('myApp').value('version', '0.1')
 
 })();
