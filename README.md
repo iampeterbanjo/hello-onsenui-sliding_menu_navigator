a8f6b0d83e77cf458c327b05020775d69687e57a
iampeterbanjo <me@iampeterbanjo.com>
2014-06-21 20:59:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Welcome to Onsen UI
+
+Here is how to run the project
+
+1 Install nodejs
+
+	http://nodejs.org/download/
+	
+2 Run `npm install`
+
+3 Run the project
+
+type: 
+	
+		node scripts/web-server.js
+
+4 Open the url
+
+		http://localhost:8000/app/index.html
---
+++ config/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function(config) {
+	config.set({
+		basePath: '../'
+
+		, files: [
+			'app/lib/onsen/js/angular/angular.js'
+			, 'app/lib/onsen/js/angular/angular-*.js'
+			, 'test/lib/angular/angular-mocks.js'
+			, 'app/js/**/*.js'
+			, 'test/unit/**/*.js'
+		]
+
+		, frameworks: ['jasmine']
+
+		, autoWatch: true
+
+		, browsers: ['PhantomJS']
+
+		, junitReporter: {
+			outputFile: 'test_out/unit.xml'
+			, suite: 'unit'
+		}
+		
+		, plugins: [
+			'karma-jasmine',
+			, 'karma-phantomjs-launcher'
+			, 'karma-chrome-launcher'
+			, 'karma-firefox-launcher'
+			, 'karma-ie-launcher'
+			, 'karma-safari-launcher'
+			, 'karma-opera-launcher'
+			, 'karma-coffee-preprocessor'
+    ]
+	})
+}
